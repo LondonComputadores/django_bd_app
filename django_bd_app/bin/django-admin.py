4eeaa9ef4b2813cd3e3a8dd1995ae2e5eede1bb5
london <london.computadores@gmail.com>
2019-09-22 13:35:27 -0300

    Starting another django project with database a recap classes!

---
+++ django_bd_app/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/c/Users/londo/Documents/digital_innov_projects/django_bd/django_bd_app/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ django_bd_proj/django_bd_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoBdAppConfig(AppConfig):
+    name = 'django_bd_app'
